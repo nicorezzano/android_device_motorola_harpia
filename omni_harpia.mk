944a83a0d2b033331fc08bc278e42c3f527f50b2
anerik <nicorezzano1@gmail.com>
2017-04-18 15:38:06 -0300

    bringup

---
+++ omni_harpia.mk
@@ -0,0 +1,26 @@
+# Copyright (C) 2016 The CyanogenMod Project
+#               2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, device/motorola/harpia/full_harpia.mk)
+
+# Boot animation
+TARGET_BOOTANIMATION_SIZE := 720x1280
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := harpia
+PRODUCT_NAME := omni_harpia
+PRODUCT_BRAND := Motorola
+PRODUCT_MANUFACTURER := Motorola
+PRODUCT_RELEASE_NAME := harpia
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
-# Copyright (C) 2016 The CyanogenMod Project
-#               2017 The LineageOS Project
+#
+# Copyright (C) 2008 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,16 +12,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-$(call inherit-product, device/motorola/harpia/full_harpia.mk)
+# This file is executed by build/envsetup.sh, and can use anything
+# defined in envsetup.sh.
+#
+# In particular, you can add lunch options with the add_lunch_combo
+# function: add_lunch_combo generic-eng
 
-# Boot animation
-TARGET_SCREEN_WIDTH := 720
-TARGET_SCREEN_HEIGHT := 1280
-
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := harpia
-PRODUCT_NAME := lineage_harpia
-PRODUCT_BRAND := Motorola
-PRODUCT_MANUFACTURER := Motorola
-PRODUCT_RELEASE_NAME := harpia
+add_lunch_combo omni_harpia-userdebug
+add_lunch_combo omni_harpia-eng
