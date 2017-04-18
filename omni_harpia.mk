81df86f95974b87631ea87c4f0121a1a59abb35b
anerik <nicorezzano1@gmail.com>
2017-04-18 18:57:22 -0300

    bringup 2

--- omni_harpia.mk
+++ omni_harpia.mk
@@ -16,7 +16,11 @@
 $(call inherit-product, device/motorola/harpia/full_harpia.mk)
 
 # Boot animation
-TARGET_BOOTANIMATION_SIZE := 720x1280
+TARGET_BOOTANIMATION_SIZE := 720x407
+
+# Twrp
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := harpia
